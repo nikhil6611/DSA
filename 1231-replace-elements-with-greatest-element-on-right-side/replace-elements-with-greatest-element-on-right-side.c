721916105d6060bc260bbeb0aca20eae30f11781
nikhil6611 <nikhiltiwari6611@gmail.com>
2025-10-02 20:17:51 +0530

    Time: 3 ms (21.11%) | Memory: 61.8 MB (57.38%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.c
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.c
@@ -2,16 +2,14 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* replaceElements(int* arr, int n, int* returnSize) {
-  int* ans = (int*)malloc(n*sizeof(int));
-  *returnSize = n;
-  ans[n-1]=-1;
-  int currleader=arr[n-1];
-  for(int i= n-2;i>=0;i--){
-    if(arr[i+1]>currleader){
-        currleader= arr[i+1];
-       
+ *returnSize = n;
+ int currleader= -1;
+ for(int i=n-1; i>=0;i--){
+    int temp = arr[i];
+    arr[i]= currleader;
+    if(temp>currleader){
+        currleader = temp;
     }
-    ans[i]= currleader;
-  }
-  return ans;
+ }
+ return arr;
 }
