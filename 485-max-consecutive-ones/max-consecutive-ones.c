7dd8dddcd62bcf474df822d89134f98fc448ed0d
nikhil6611 <nikhiltiwari6611@gmail.com>
2025-10-03 23:09:59 +0530

    Time: 4 ms (7.28%) | Memory: 12.5 MB (92.72%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.c
@@ -0,0 +1,16 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+   int count =0;
+   int max=0;
+   for(int i=0;i<numsSize;i++){
+    if(nums[i]==1){
+        count++;
+    }else{
+        if(count>max){
+            max=count;
+        }
+        count=0;
+    }
+   }
+
+   return count>max?count:max;
+}
